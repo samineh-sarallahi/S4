e0c0a1ffe49fd61737480d7a12fe3e9e89d6f107
samineh-sarallahi <154461112+samineh-sarallahi@users.noreply.github.com>
2024-01-08 22:54:01 +0330

    Add files via upload

---
+++ s4-prime numbers.py
@@ -0,0 +1,7 @@
+p = int(input('enter a number:'))
+for i in range(1,p):
+    if p == 1:
+        print("it is not prim number")
+    elif p>1:
+        p % 2 == 0 or p % 3 == 0 or p % 5 ==0
+print(" it is prim number")
