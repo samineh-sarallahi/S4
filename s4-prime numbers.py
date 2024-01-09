9ada47fefa9aa7515716c4bc47077e21345608f3
samineh-sarallahi <154461112+samineh-sarallahi@users.noreply.github.com>
2024-01-09 16:43:04 +0330

    Update s4-prime numbers.py

--- s4-prime numbers.py
+++ s4-prime numbers.py
@@ -1,10 +1,12 @@
-p = int(input('enter a number:'))
-if p>1 :
-    for i in range(2,p):
-        if p%i == 0:
-            print(p, "is not a prime number")
-    
-    else:
-            print(p, "is a prime number")
+num = int(input("Enter a number:"))
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2,num):
+        if (num % i) == 0:
+            print(num,"is not a prime number")
+        else:
+            print(num,"is a prime number")
 
 
